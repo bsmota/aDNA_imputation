de8c4239e0300406337037082dd95eced1a5a3d0
bsmota <69356354+bsmota@users.noreply.github.com>
2023-02-08 16:04:33 +0100

    Create step2_impute.sh

---
+++ step1_imputation/step2_impute.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#SBATCH --array=1-X
+#SBATCH --job-name ImpLC
+#SBATCH --account=amalaspi_popgen
+#SBATCH --nodes 1
+#SBATCH --ntasks 1
+#SBATCH --cpus-per-task 8
+#SBATCH --export=NONE
+#SBATCH --mem 15G
+#SBATCH --output=impute_%a.out
+#SBATCH --error=impute_%a.er
+
+#this script allows to impute the X chunks in parallel
+
+#software location
+IMP=/GLIMPSE-v1.1.1/phase/bin/GLIMPSE_phase
+
+#chunks coordinates and info
+LST=/coordinates/referencel.panel.chr${CHR}.txt
+
+DATA=$(cat $LST | head -n $SLURM_ARRAY_TASK_ID | tail -n 1)
+
+#chunk number
+IDG=$(echo $DATA | cut -d" " -f1)
+#chromosome
+CHR=$(echo $DATA | cut -d" " -f2)
+
+IREG=$(echo $DATA | cut -d" " -f3)
+OREG=$(echo $DATA | cut -d" " -f4)
+
+#Reference panel
+REF=/work/FAC/FBM/DBC/amalaspi/popgen/shared_ressources/1000Genomes_umich/1000G_umich_genotypes/1000GP_nygc_umich_chr${CHR}_snps_only_genotypes.vcf.gz
+
+#genotype likelihoods
+GLS=/calls/chr${CHR}/chr${CHR}.merged.bcf
+
+#genetic map (if build 37 of the reference genome)
+MAP=/GMAP_shapeit4/chr${CHR}.b37.gmap.gz
+
+#output file
+OUTD=/output_imputation/chr$CHR.reg$IDG.bcf
+
+#Run GLIMPSE_phase to impute variants in chunk
+$IMP --input $GLS --reference $REF --input-region $IREG --output-region $OREG --map $MAP --output $OUTD
